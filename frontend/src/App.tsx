78fd7e766c282c9582ccc5095ffcfd680b1b0c84
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-03-23 18:52:32 +0100

    Creating routes for login, register, adding react toastify to show the error messages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/HomePage'
 import Navbar from './components/Navbar'
-import JobsCard from './components/JobsCard/JobsCard';
+// import JobsCard from './components/JobsCard/JobsCard'
+import Login from './components/Auth/Login'
+import Signup from './components/Auth/Signup'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: React.FC = () => (
   <div className="container">
     <Navbar/>
-    <JobsCard/>
+    <ToastContainer/>
   <BrowserRouter>
 		<Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
 		</Routes>
     </BrowserRouter>
     </div>
