4c25eb59c50a66ca91a43f9b8a57e41638f7ccac
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-09 21:33:21 +0200

    Cors configuration

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jobboard-gold.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://jobboard-gold.vercel.app/*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
