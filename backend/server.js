69b0cc8bda6b79f09c4632b038114a0221f3d23c
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-09 22:10:46 +0200

    Allowing methods

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(cors({
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://jobboard-gold.vercel.app/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods, GET,OPTIONS,PATCH,DELETE,POST,PUT")
   next();
 });
 
