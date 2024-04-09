2d989180d139623bd08ae065579f095d2f041608
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-09 22:21:30 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -15,19 +15,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors({
-  origin: 'https://jobboard-gold.vercel.app/',
-  // header: 'Access-Control-Allow-Origin", "https://jobboard-gold.vercel.app/',
+  origin: 'https://jobboard-gold.vercel.app',
   methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
-  credentials: true, //included credentials as true
-
+  credentials: true
 }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jobboard-gold.vercel.app/");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods, GET,OPTIONS,PATCH,DELETE,POST,PUT")
-  next();
-});
+
 
 app.use('/users', userRoutes);
 app.use('/jobs', jobsRoutes);
