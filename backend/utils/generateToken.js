95da26306f3da039391b2cb11ed968e985545a9b
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-12 20:12:25 +0200

    Trying in a little different way

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,17 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-
 const generateToken = (res, userId) => {
-const token = jwt.sign({userId }, process.env.JWT_SECRET, {
-    expiresIn: '30d'
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
     });
 
-    res.cookie('jwt', token, {
+    const cookieOptions = {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    };
 
-}
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true; 
+        cookieOptions.sameSite = 'None'; 
+    }
 
-module.exports = generateToken
+    res.cookie('jwt', token, cookieOptions);
+};
+
+module.exports = generateToken;
