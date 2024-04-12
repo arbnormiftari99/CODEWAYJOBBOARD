141237e8ac4220d91752f4da16ecb4ce14014c6c
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-12 19:12:01 +0200

    .

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const token = jwt.sign({userId }, process.env.JWT_SECRET, {
         httpOnly: true,
         // secure: process.env.NODE_ENV !== 'development',
         secure: true,
-        sameSite: 'None',
+        sameSite: 'Lax',
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
