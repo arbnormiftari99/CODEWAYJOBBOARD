0d02481bca7fe88e6ff01fb63102c266e3c5f5f8
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-12 18:41:54 +0200

    Fixing the bug with token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,9 +8,8 @@ const token = jwt.sign({userId }, process.env.JWT_SECRET, {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        // secure: process.env.NODE_ENV !== 'development',
-        secure: true,
-        sameSite: 'strict',
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
