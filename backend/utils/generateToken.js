d6512935f9d4fa83863357a9ad70c425e72faa00
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-15 10:48:03 +0200

    Testing in production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,10 +9,10 @@ const token = jwt.sign({userId }, process.env.JWT_SECRET, {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'Strict',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         domain: 'https://jobboard-gold.vercel.app',
         path: '/',
-        sameSite: 'Strict'
     });
 
 }
