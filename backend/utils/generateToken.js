74f7c3ea3b78951cdfecdab383e0b2e8fd9fc33d
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-15 10:28:46 +0200

    Testing in production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,23 +1,19 @@
 const jwt = require('jsonwebtoken');
 
+
 const generateToken = (res, userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
+const token = jwt.sign({userId }, process.env.JWT_SECRET, {
+    expiresIn: '30d'
     });
 
-    const cookieOptions = {
+    res.cookie('jwt', token, {
         httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         domain: 'https://jobboard-gold.vercel.app',
         path: '/'
-    };
+    });
 
-    if (process.env.NODE_ENV === 'production') {
-        cookieOptions.secure = true; 
-        // cookieOptions.sameSite = 'None'; 
-    }
+}
 
-    res.cookie('jwt', token, cookieOptions);
-};
-
-module.exports = generateToken;
+module.exports = generateToken
